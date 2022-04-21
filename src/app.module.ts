f04cc39df64a4688e1e48c5f13e57cc781cc0768
Avinash Rijal <avinashrijal@gmail.com>
2022-04-21 11:27:52 +0545

    feat: setup typeorm for postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'iluvcoffee',
+      // this help load modules automatically instead of specifying in entities array
+      autoLoadEntities: true,
+      // ensures our typeorm entities are sync with the database everytime we run our application. This is great for development. !!!Disable this on production!!!
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
