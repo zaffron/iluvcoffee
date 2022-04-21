ca88a69ce6750b5af590fa9fff8d86a8dd53bd12
Avinash Rijal <avinashrijal@gmail.com>
2022-04-21 11:41:28 +0545

    feat: create typeorm entity

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,9 +1,12 @@
+import { Coffee } from './entities/coffee.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Coffee])],
   controllers: [CoffeesController],
   providers: [CoffeesService],
 })
--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity() // sql table === 'coffee' if you want to use 'plural work just type that inside the entity method like @Entity('coffees')
 export class Coffee {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   brand: string;
+
+  @Column('json', { nullable: true })
   flavors: string[];
 }
