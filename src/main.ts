01e4307022924cdf8e6af1bd7628b340ba59cb7d
Avinash Rijal <avinashrijal@gmail.com>
2022-04-21 11:11:38 +0545

    feat: add transform feature

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,10 @@ async function bootstrap() {
       whitelist: true,
       // Enable this is you want to throw error when unwanted keys are sent by the user
       forbidNonWhitelisted: true,
+      // auto transform and handle type conversion for incoming request payloads
+      // also performs primitive type conversion like bool and number
+      // But this comes with the cost of performance, so make sure you have negligible performance issue before proceeding
+      transform: true,
     }),
   );
   await app.listen(3000);
