380aebaf7e1204871bc3cf739ec4e1ab8d5fbde4
Avinash Rijal <avinashrijal@gmail.com>
2022-04-21 09:04:37 +0545

    feat: add response status code to the coffee controller

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post } from '@nestjs/common';
+
+@Controller('coffees')
+export class CoffeesController {
+  @Get()
+  findAll() {
+    return 'This action returns all coffees';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action return #${id} coffee`;
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.GONE)
+  create(@Body() body) {
+    return body;
+  }
+}
