dcf2f7ae7a3f5de5f273f336f10910b43ea7170f
Avinash Rijal <avinashrijal@gmail.com>
2022-04-21 09:08:04 +0545

    feat: remove native response for preferred nest handlers

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -6,14 +6,13 @@ import {
   HttpStatus,
   Param,
   Post,
-  Res,
 } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get()
-  findAll(@Res() response) {
-    response.status(200).send('This action returns all coffees');
+  findAll() {
+    return 'This action returns all coffees';
   }
 
   @Get(':id')
