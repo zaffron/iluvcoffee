cf5951311aa0f1eff21f63f6a05a729f2e35bf60
Avinash Rijal <avinashrijal@gmail.com>
2022-04-22 13:43:14 +0545

    feat: add value based providers for coffees service

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -8,10 +8,13 @@ import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { Flavor } from './entities/flavor.entity';
 
+class MockCoffeesService {}
+
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
-  providers: [CoffeesService],
+  // Whenever we call CoffeesService new MockCoffeeService will be provided
+  providers: [{ provide: CoffeesService, useValue: new MockCoffeesService() }],
   exports: [CoffeesService],
 })
 export class CoffeesModule {}
