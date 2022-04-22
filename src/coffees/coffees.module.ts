eef8427cf220c9bfee41450442506dfd60adf161
Avinash Rijal <avinashrijal@gmail.com>
2022-04-22 13:52:25 +0545

    feat: add class based dynamic provide resolver

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -8,13 +8,23 @@ import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { Flavor } from './entities/flavor.entity';
 
+class ConfigService {}
+class DevelopmentConfigService {}
+class ProductionConfigService {}
+
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
   // Whenever we call CoffeesService new MockCoffeeService will be provided
   providers: [
     CoffeesService,
-    { provide: 'COFFEE_BRANDS', useValue: ['buddy brew', 'nescafe'] },
+    {
+      provide: ConfigService,
+      useClass:
+        process.env.NODE_ENV === 'development'
+          ? DevelopmentConfigService
+          : ProductionConfigService,
+    },
   ],
   exports: [CoffeesService],
 })
