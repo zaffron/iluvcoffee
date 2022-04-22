bd727e4f9e345faec865e6f2127066a29667ee7d
Avinash Rijal <avinashrijal@gmail.com>
2022-04-22 13:58:00 +0545

    feat: use class factory for providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,23 +1,38 @@
+import { COFFEE_BRANDS } from './coffees.constants';
 import { Coffee } from './entities/coffee.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Module } from '@nestjs/common';
+import { Module, Injectable } from '@nestjs/common';
 
 import { Event } from 'src/events/entities/event.entity';
 
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { Flavor } from './entities/flavor.entity';
+import { create } from 'domain';
 
 class ConfigService {}
 class DevelopmentConfigService {}
 class ProductionConfigService {}
 
+@Injectable()
+export class CoffeeBrandsFactory {
+  create() {
+    return ['buddy brew', 'nescafe'];
+  }
+}
+
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
-  // Whenever we call CoffeesService new MockCoffeeService will be provided
   providers: [
     CoffeesService,
+    CoffeeBrandsFactory,
+    {
+      provide: COFFEE_BRANDS,
+      useFactory: (brandsFactory: CoffeeBrandsFactory) =>
+        brandsFactory.create(),
+      inject: [CoffeeBrandsFactory],
+    },
     {
       provide: ConfigService,
       useClass:
