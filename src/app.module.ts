faac957c11edde292e973918ec62728f478ec16e
Avinash Rijal <avinashrijal@gmail.com>
2022-04-25 17:15:32 +0545

    feat: binding validation pipe to app pipe on app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import * as Joi from '@hapi/joi';
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { APP_PIPE } from '@nestjs/core';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -40,6 +41,12 @@ import appConfig from './config/app.config';
     DatabaseModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe,
+    },
+  ],
 })
 export class AppModule {}
